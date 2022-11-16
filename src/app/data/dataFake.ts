07b602eb0e3f3a7ab13180c26f591c4fcefb1ea7
Yago <yagofer12@gmail.com>
2022-11-16 20:27:15 -0300

    finished

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Novo homem de ferro!",
+    "description": "Marvel anuncia um novo homem de ferro, dessa vez com as Joias do Infinito! Quando o multiverso se abriu surgiu uma variante do Homem de Ferro mais poderosa de todas.",
+    "photoCover": "https://marvelucm.com/wp-content/uploads/2022/01/jpg1-819x1024.jpg",
+  },
+  {
+    "id": "2",
+    "title": "Adão Negro é libertado no mundo moderno, e pretende utilizar os poderes ganhos de deuses egípcios para exercitar seu peculiar conceito de justiça. Além de marcar a estreia de Dwayne Johnson nas produções cinematográficas da DC.",
+    "description": "Adão Negro é o filme solo do anti-herói, baseado no personagem em quadrinhos Black Adam (Dwayne Johnson) da DC Comics, grande antagonista de Shazam!, tendo no longa, sua história de origem explorada, e revelando seu passado de escravo no país Kahndaq. Nascido no Egito Antigo, o anti-herói tem super força, velocidade, resistência, capacidade de voar e de disparar raios. Alter ego de Teth-Adam e filho do faraó Ramsés II, Adão Negro foi consumido por poderes mágicos e transformado em um feiticeiro. Grande inimigo de Shazam! nas HQs, apesar dele acreditar em seu pontecial e, inclusive, oferecê-lo como um guerreiro do bem, Adão Negro acaba usando suas habilidades especiais para o mal. O anti-herói em busca de redenção ou um herói que se tornou vilão, pode ser capaz de destruir tudo o que estiver pela frente - ou de encontrar seu caminho.",
+    "photoCover": "https://recreio.uol.com.br/media/uploads/dc_comics/adao_negro_hqs_capa.jpg",
+  },
+  {
+    "id": "3",
+    "title": "A produção de 2022, o protagonista enfrentará o vilão Senhor Cérebro, um supervilão alienígena. Nascido em Vênus, o extraterreste é uma espécie de larva, e foi o escolhido para infiltrar-se e dominar a Terra.",
+    "description": "O segundo filme se chamará Shazam! Fury of the Gods, que em tradução livre significa “Shazam! A Fúria dos Deuses”. Com isso, nós podemos imaginar que os deuses que dão poder ao personagem estarão furiosos com algo – muito provavelmente, a forma como Billy Batson usa seus dons para se divertir. Porém, pelos detalhes revelados da trama, parece que o herói está em maus lençóis.Sabemos que Asher Angel e Zachary Levi retornarão para interpretar o herói (Asher como Billy, Zachary como Shazam), e de acordo com novas imagens dos bastidores, nós podemos ver que o personagem ganhará um trato no visual, com um novo traje muito mais próximo dos quadrinhos. O raio em seu peito mudou, há mais textura em sua roupa e até mesmo a capa foi modificada para dar um ar mais “heroico” ao Shazam.Quanto aos vilões, teremos uma verdadeira cabala de novas rivais para o herói. Helen Mirren e Lucy Liu foram confirmadas como Hespera e Kalypso, respectivamente, duas personagens que não aparecem nas HQs mas que, no filme, devem cumprir o papel de filhas de Atlas, uma das divindades que dá poderes ao Shazam. Ainda se espera que novos vilões sejam confirmados.",
+    "photoCover": "https://epipoca.com.br/wp-content/uploads/2021/10/Shazam-4-featured-1280x720-1-1200x900.jpg",
+  },
+  {
+    "id": "4",
+    "title": "Na sequência de “Pantera Negra” (2018), Wakanda precisa se defender de interferências externas após a morte do rei T’Challa. Enquanto a nação tenta reerguer-se, o vilão da trama surge: Namor.",
+    "description": "T'Challa, rei de Wakanda, morre de uma doença que sua irmã Shuri acredita que poderia ter sido curada pela 'erva em forma de coração'. Shuri tem tentado recriar sinteticamente a erva depois que foi destruída por Killmonger,[N 1] mas não teve sucesso.Um ano depois, Wakanda está sob pressão de outros países para compartilhar seu vibranium, com algumas partes tentando roubá-lo à força. Ramonda implora a Shuri que continue sua pesquisa sobre a erva em forma de coração, na esperança de criar um novo Pantera Negra que defenda Wakanda, mas ela se recusa, pois acredita que o Pantera Negra é uma figura do passado. A CIA usa uma nova máquina de detecção de vibranium em uma expedição a um potencial depósito de vibranium debaixo d'água. Toda a equipe de busca é então atacada e morta por Namor e seu povo de pele azul e respiração aquática, mas a CIA acredita que Wakanda foi responsável. Namor vai até Ramonda e Shuri, burlando facilmente a segurança avançada de Wakanda. Ele culpa Wakanda pela busca de vibranium e dá a eles um ultimato – encontre e dê a ele a cientista responsável pela máquina de detecção de vibranium, ou ele atacará Wakanda.",
+    "photoCover": "https://avancegames.com.br/wp-content/uploads/2022/09/Pantera-Negra-2-Wakanda-Forever-Shuri-Okoye-Nakia-Avance-Games-1200x900.jpg",
+  }
+]
